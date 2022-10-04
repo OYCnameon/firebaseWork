cc2b1ab665607f268a1200c4dfe61f41e956071b
OYCnameon <nameonb0507@gmail.com>
2022-10-04 17:33:21 +0900

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-
+import AppShell from "./AppShell";
 class App extends React.Component {    
     render() {        
         return (            
-        <div>                
-            <h3>test</h3>
-        </div>        
+            <AppShell/>     
         );    
     }}
 export default App;
